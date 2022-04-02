f2dbf62eea8a5fd751514112f6d1d07aef1d3671
侯杰 <houjie@fosung.com>
2022-04-02 09:12:12 +0800

    libshadowlayout圆角阴影库

--- app/src/main/java/com/fosung/lighthouse/fosunglibs/MainActivity.java
+++ app/src/main/java/com/fosung/lighthouse/fosunglibs/MainActivity.java
@@ -1,9 +1,11 @@
 package com.fosung.lighthouse.fosunglibs;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.geek.libshadowlayout.ShadowMainActivity;
 import com.pgyer.pgyersdk.PgyerSDKManager;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,9 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new PgyerSDKManager.Init()
-                .setContext(getApplicationContext()) //设置上下问对象
-                .start();
+        Intent intent3 = new Intent(this, ShadowMainActivity.class);
+        startActivity(intent3);
+//        new PgyerSDKManager.Init()
+//                .setContext(getApplicationContext()) //设置上下问对象
+//                .start();
 //        startActivity(new Intent(MainActivity.this, ScanAct2.class));
 
 
