bd497ae921b7c1aa71c86eec639bd120d4aba752
侯杰 <houjie@fosung.com>
2022-04-02 17:50:54 +0800

    视频压缩库和阴影库app_name报错修改

--- app/src/main/java/com/fosung/lighthouse/fosunglibs/MainActivity.java
+++ app/src/main/java/com/fosung/lighthouse/fosunglibs/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.geek.libshadowlayout.ShadowMainActivity;
-import com.pgyer.pgyersdk.PgyerSDKManager;
+import com.vincent.videocompressor.activity.VideoComPressorActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent3 = new Intent(this, ShadowMainActivity.class);
+
+        Intent intent3 = new Intent(this, VideoComPressorActivity.class);
         startActivity(intent3);
+//        Intent intent3 = new Intent(this, ShadowMainActivity.class);
+//        startActivity(intent3);
 //        new PgyerSDKManager.Init()
 //                .setContext(getApplicationContext()) //设置上下问对象
 //                .start();
