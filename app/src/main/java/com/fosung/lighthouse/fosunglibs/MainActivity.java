8b0a5bbfef35dcda2d383ed1da88149021c9f540
liangxiao6 <liangxiao6@live.com>
2022-04-27 15:52:50 +0800

    merger

--- app/src/main/java/com/fosung/lighthouse/fosunglibs/MainActivity.java
+++ app/src/main/java/com/fosung/lighthouse/fosunglibs/MainActivity.java
@@ -1,26 +1,32 @@
 package com.fosung.lighthouse.fosunglibs;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Base64;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.geek.libshadowlayout.ShadowMainActivity;
-import com.pgyer.pgyersdk.PgyerSDKManager;
-
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent3 = new Intent(this, ShadowMainActivity.class);
-        startActivity(intent3);
+//        Intent intent3 = new Intent(this, ShadowMainActivity.class);
+//        startActivity(intent3);
 //        new PgyerSDKManager.Init()
 //                .setContext(getApplicationContext()) //设置上下问对象
 //                .start();
 //        startActivity(new Intent(MainActivity.this, ScanAct2.class));
-
+        tv1 = findViewById(R.id.tv1);
+        String strBase64 = Base64.encodeToString("梁肖".getBytes(), Base64.DEFAULT);// 编码
+//        tv1.setText(EncodeUtils.base64Encode2String("梁肖".getBytes()));
+        String str2 = new String(Base64.decode(strBase64.getBytes(), Base64.DEFAULT));// 解码
+        tv1.setText(strBase64 + "," + str2);
 
     }
+
+
 }
