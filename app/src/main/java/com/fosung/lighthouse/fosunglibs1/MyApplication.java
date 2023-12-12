7bc2aaabaf0df04d31c417c3d56b273eaa07cb30
liangxiao <liangxiao6@live.com>
2023-12-12 15:05:15 +0800

    merge2.3.2

--- app/src/main/java/com/fosung/lighthouse/fosunglibs1/MyApplication.java
+++ app/src/main/java/com/fosung/lighthouse/fosunglibs1/MyApplication.java
@@ -7,12 +7,7 @@ import androidx.multidex.MultiDex;
 
 import com.blankj.utilcode.util.SPUtils;
 import com.blankj.utilcode.util.Utils;
-<<<<<<< HEAD
 import com.geek.libutils.data.MmkvUtils;
-=======
-import com.yanbo.libscreen.VApplication;
-import com.yanbo.libscreen.mmkv.MmkvUtils;
->>>>>>> 41302ece0d5a7b1f624c5d2c8f645741b38a4df2
 
 import me.jessyan.autosize.AutoSize;
 import me.jessyan.autosize.AutoSizeConfig;
